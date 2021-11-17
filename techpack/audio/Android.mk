316cec062fcd7d1f820b3474eacf68fd13664d2c
SagarMakhar <sagarmakhar@gmail.com>
2021-11-17 17:14:32 +0000

    techpack/audio: Import realme changes
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- techpack/audio/Android.mk
+++ techpack/audio/Android.mk
@@ -1,7 +1,7 @@
 # Android makefile for audio kernel modules
 MY_LOCAL_PATH := $(call my-dir)
 
-ifeq ($(call is-board-platform-in-list,msm8953 sdm845 sdm670 sdm660 qcs605 msmnile $(MSMSTEPPE) $(TRINKET)),true)
+ifeq ($(call is-board-platform-in-list,msm8953 sdm845 sdm670 sdm660 qcs605 msmnile  $(MSMSTEPPE) $(TRINKET)),true)
 UAPI_OUT := $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/include
 
 $(shell mkdir -p $(UAPI_OUT)/linux;)
@@ -24,7 +24,7 @@ include $(MY_LOCAL_PATH)/asoc/codecs/Android.mk
 include $(MY_LOCAL_PATH)/asoc/codecs/wcd934x/Android.mk
 endif
 
-ifeq ($(call is-board-platform-in-list, atoll),true)
+ifeq ($(call is-board-platform-in-list, atoll ),true)
 UAPI_OUT := $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/4.0/include
 
 $(shell mkdir -p $(UAPI_OUT)/linux;)
@@ -57,7 +57,7 @@ $(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codec
 include $(MY_LOCAL_PATH)/asoc/codecs/wcd937x/Android.mk
 endif
 
-ifeq ($(call is-board-platform-in-list, atoll),true)
+ifeq ($(call is-board-platform-in-list, atoll ),true)
 $(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/4.0/asoc/codecs/bolero/Module.symvers)
 include $(MY_LOCAL_PATH)/4.0/asoc/codecs/bolero/Android.mk
 $(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/4.0/asoc/codecs/wcd937x/Module.symvers)
@@ -77,3 +77,41 @@ ifeq ($(call is-board-platform-in-list,msmnile),true)
 $(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/wcd9360/Module.symvers)
 include $(MY_LOCAL_PATH)/asoc/codecs/wcd9360/Android.mk
 endif
+
+#ifdef OPLUS_ARCH_EXTENDS
+#Guian.Chen@MULTIMEDIA.AUDIODRIVER.CODEC, 2018/07/31, Add for aw882xx codec
+ifeq ($(call is-board-platform-in-list, atoll),true)
+$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/4.0/asoc/codecs/aw882xx/Module.symvers)
+include $(MY_LOCAL_PATH)/4.0/asoc/codecs/aw882xx/Android.mk
+endif
+#endif /* OPLUS_ARCH_EXTENDS */
+
+#ifdef OPLUS_ARCH_EXTENDS
+#Kaiqin.Huang@RM.MM.AudioDriver.Codec, 2020/07/29, Add for sia81xx codec
+ifeq ($(call is-board-platform-in-list, atoll),true)
+$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/4.0/asoc/codecs/sia81xx/Module.symvers)
+include $(MY_LOCAL_PATH)/4.0/asoc/codecs/sia81xx/Android.mk
+endif
+#endif /* OPLUS_ARCH_EXTENDS */
+
+#ifdef OPLUS_ARCH_EXTENDS
+#Wan.li@MULTIMEDIA.AUDIODRIVER.CODEC, 2020/09/27, Add for max98937 codec
+ifeq ($(call is-board-platform-in-list,msmnile $(MSMSTEPPE)),true)
+$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/max989xx/Module.symvers)
+include $(MY_LOCAL_PATH)/asoc/codecs/max989xx/Android.mk
+endif
+#endif /* OPLUS_ARCH_EXTENDS */
+
+#ifdef OPLUS_ARCH_EXTENDS
+#Jianfeng.Qiu@MULTIMEDIA.AUDIODRIVER.CODEC, 2018/04/20, Add for ak43xx codec
+ifeq ($(call is-board-platform-in-list, $(MSMSTEPPE)),true)
+include $(MY_LOCAL_PATH)/asoc/codecs/ak4376/Android.mk
+endif
+#endif /* OPLUS_ARCH_EXTENDS */
+
+#ifdef OPLUS_ARCH_EXTENDS
+#Yongzhi.Zhang@MULTIMEDIA.AUDIODRIVER.CODEC, 2019/07/20, add for dbmdx
+ifeq ($(call is-board-platform-in-list, $(MSMSTEPPE)),true)
+include $(MY_LOCAL_PATH)/asoc/codecs/dbmdx/Android.mk
+endif
+#endif /* OPLUS_ARCH_EXTENDS */
