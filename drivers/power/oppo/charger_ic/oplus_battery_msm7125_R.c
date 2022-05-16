ef5e50b06edc8a63c5d178eeceea77634f697d53
Mayur <ultramayur123@gmail.com>
2022-05-16 06:25:13 +0530

    treewide: fix error  error: a function declaration without a prototype is deprecated in all versions of C [-Werror,-Wstrict-prototypes]
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- drivers/power/oppo/charger_ic/oplus_battery_msm7125_R.c
+++ drivers/power/oppo/charger_ic/oplus_battery_msm7125_R.c
@@ -16914,7 +16914,7 @@ int oplus_chg_get_charger_subtype(void)
 }
 
 extern int oplus_pdo_select(int vbus_mv, int ibus_ma);
-int oplus_chg_set_pd_config()
+int oplus_chg_set_pd_config(void)
 {
 	int ret = -1;
 	struct oplus_chg_chip *chip = g_oplus_chip;
@@ -17065,7 +17065,7 @@ int oplus_chg_enable_qc_detect(void)
 	return ret;
 }
 
-int oplus_chg_set_qc_config()
+int oplus_chg_set_qc_config(void)
 {
 	int ret = -1;
 	struct smb_charger *chg = NULL;
